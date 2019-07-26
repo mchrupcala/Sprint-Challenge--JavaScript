3d041a2f7c1936e4b8e5da63ae2f21ff8e15afa4
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-07-26 11:59:02 -0400

    Finished the dinosaur objects and the first three requests in objects-array.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -4,7 +4,7 @@
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
 
-class CuboidMaker {
+class CuboidMaker1 {
     constructor(attrs) {
     this.length = attrs.length;
     this.width = attrs.width;
@@ -24,7 +24,7 @@ class CuboidMaker {
     Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
   */
   
-  const cuboid = new CuboidMaker1({
+  const cuboid1 = new CuboidMaker1({
     length: 4,
     width: 5,
     height: 5
@@ -33,7 +33,7 @@ class CuboidMaker {
 
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid1.volume()); // 100
+console.log(cuboid1.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,25 +6,60 @@
   object name, diet, weight, length, period
 */
 
+class Dino {
+  constructor(attrs) {
+    this.type = attrs.type;
+    this.diet = attrs.diet;
+    this.weight = attrs.weight;
+    this.length = attrs.length;
+    this.era = attrs.era; 
+  }
+}
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+
+const tRex = new Dino ({
+  type: "Tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  era: "Late Cretaceous"
+})
+
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stego = new Dino ({
+  type: "Stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  era: "Late Jurassic"
+})
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const raptor = new Dino ({
+  type: "Velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  era: "Late Cretaceous"
+})
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tRex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(raptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stego.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tRex.era);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
@@ -46,11 +81,14 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
+/* Request 1: Create a new array called universities that contains all the universities in the graduates array. 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+graduates.forEach(function(x){
+  universities.push(x.university);
+})
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -59,11 +97,16 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(function(y){
+  contactInfo.push(`${y.first_name} ${y.email}`);
+})
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = graduates.filter(function(z){
+  return z.university.includes("Uni");
+})
 console.log(uni);
 
 
