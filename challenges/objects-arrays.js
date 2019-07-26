494d32d2f5025773a948b10864e193b6e93ea990
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-07-26 12:12:33 -0400

    Finished objects.arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -132,7 +132,12 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+  zooAnimals.forEach(function(names){
+    animalNames.push(`${names.animal_name}, ${names.scientific_name}`);
+  })
 console.log(animalNames);
+
+
 
 /* Request 2: .map()    
 
@@ -140,23 +145,33 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(function(lower){
+    return lower.animal_name.toLowerCase();
+  })
 console.log(lowerCase); 
+
 
 /* Request 3: .filter() 
 
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const lowerPopulation = [];
+const lowerPopulation = zooAnimals.filter(function(pop){
+  return pop.population < 5;
+})
 console.log(lowerPopulation);
+
+
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce(function(acc, total){
+  return acc += total.population;
+}, 0);
+
 console.log(populationTotal);
 
 
